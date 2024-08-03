34b989f2d8bb1f663653eaa1d78af739d7a8dabc
Priyanshu-aggarwal8 <agarwalpriyanshu812@gmail.com>
2024-08-03 14:54:04 +0530

    commit

--- src/Chatbot.js
+++ src/Chatbot.js
@@ -27,6 +27,36 @@ const ChatBot = () => {
       case 'how are you?':
         responseText = 'I am a bot, so I am always fine! How about you?';
         break;
+      case 'what are you doing?':
+        responseText = 'Right now I am trying to assist you! How about you?';
+        break;
+      case 'what is your name?':
+        responseText = 'I am a chatbot created by you. You can call me ChatBot!';
+        break;
+      case 'what can you do?':
+        responseText = 'I can chat with you and answer simple questions. Try asking me something!';
+        break;
+      case 'tell me a joke':
+        responseText = 'Why did the scarecrow win an award? Because he was outstanding in his field!';
+        break;
+      case 'what is the weather like?':
+        responseText = 'I am not connected to the internet, so I can\'t provide weather updates.';
+        break;
+      case 'goodbye':
+        responseText = 'Goodbye! Have a great day!';
+        break;
+      case 'thank you':
+        responseText = 'You\'re welcome!';
+        break;
+      case 'what is your favorite color?':
+        responseText = 'I like blue. It\'s calm and cool!';
+        break;
+      case 'how old are you?':
+        responseText = 'I am timeless, as I exist in the digital world!';
+        break;
+      case 'where do you live?':
+        responseText = 'I live in the cloud, ready to assist you anytime!';
+        break;
       default:
         responseText = "I'm not sure how to respond to that.";
         break;
